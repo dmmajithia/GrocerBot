54808779be49e36ed39bc2a3c2a50723f83479df
Dhawal Majithia <deviolxd@icloud.com>
2017-09-12 12:32:44 -0700

    bot update

--- app.js
+++ app.js
@@ -66,7 +66,7 @@ function processPostback(event) {
         name = bodyObj.first_name;
         greeting = "Hi " + name + ". ";
       }
-      var message = greeting + "My name is SP Movie Bot. I can tell you various details regarding movies. What movie would you like to know about?";
+      var message = greeting + "My name is Grocer. I can keep track of your groceries. What does your kitchen have today? Or should we start off with a grocery list?";
       sendMessage(senderId, {text: message});
     });
   }
