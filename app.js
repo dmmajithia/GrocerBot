dfb5e387b9ba0fba7a2026f3023173ada02f2471
Dhawal Majithia <deviolxd@icloud.com>
2017-09-14 13:10:52 -0700

    setup list

--- app.js
+++ app.js
@@ -66,7 +66,8 @@ function processMessage(event){
     	var message = event.message;
     	var userID = event.sender.id;
     	// You may get a text or attachment but not both
-    	if (message.quick_reply.payload) {
+    	console.log(message);
+    	if (message.quick_reply[0].payload) {
     		processQuickReply(userID, message.quick_reply[0].payload);
     		return;
     	}
